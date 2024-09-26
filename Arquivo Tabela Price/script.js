23a08de5b5e19074fb49f0ca93947a1f026ad60b
caiopalmardemelo <159266607+caiopalmardemelo@users.noreply.github.com>
2024-09-26 12:14:11 -0300

    Add files via upload

--- Arquivo Tabela Price/script.js
+++ Arquivo Tabela Price/script.js
@@ -1,16 +1,23 @@
-function calcula(){
-    const valor = document.getElementById('valor').value;
-    const taxa = document.getElementById('taxa').value;
-    const parcela = document.getElementById('parcela').value;
-    const valor_parcela = (valor*Math.pow(taxa + 1,parcela)/(Math.pow(taxa + 1,parcela)-1));
-    const corpo_tabela = document.querySelector('#tabela tbody');
-}
+function clcr(){
+    let vlr = parseFloat(document.getElementById('vlr').value);
+    let txa = parseFloat(document.getElementById('txa').value);
+    let prcl = parseFloat(document.getElementById('prcl').value);
+    let jrs = vlr * txa;
+    let iem = (vlr * Math.pow(txa+1, prcl) * txa)/(Math.pow(txa+1, prcl)-1);
+    let idem = iem - jrs;
+    let crp_tbl = document.querySelector('#tabela tbody');
 
-for (let i=1,i<=parcela;i++){
-    <tr>
-        <td>$(i)</td>
-        <td>$(valor_parcela)</td>
-    </tr>
+    for (let i=1;i<=prcl;i++){
+        let lnha =`
+            <tr>
+                <td>${i}</td>
+                <td>${iem.toFixed(2)}</td>
+                <td>${jrs.toFixed(2)}</td>
+                <td>${idem.toFixed(2)}</td>
+                <td>${vlr.toFixed(2)}</td>
+            </tr>
+            `;
 
-    corpo_tabela.innerHTML=linha;
+        crp_tbl.innerHTML += lnha;
+    }
 }
