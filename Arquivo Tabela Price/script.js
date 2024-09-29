1123cd4c63a5c4653140302d48d3c99628bdc9d3
caiopalmardemelo <159266607+caiopalmardemelo@users.noreply.github.com>
2024-09-29 20:30:22 -0300

    Add files via upload

--- Arquivo Tabela Price/script.js
+++ Arquivo Tabela Price/script.js
@@ -1,23 +1,41 @@
-function clcr(){
-    let vlr = parseFloat(document.getElementById('vlr').value);
-    let txa = parseFloat(document.getElementById('txa').value);
-    let prcl = parseFloat(document.getElementById('prcl').value);
-    let jrs = vlr * txa;
-    let iem = (vlr * Math.pow(txa+1, prcl) * txa)/(Math.pow(txa+1, prcl)-1);
-    let idem = iem - jrs;
-    let crp_tbl = document.querySelector('#tabela tbody');
 
-    for (let i=1;i<=prcl;i++){
-        let lnha =`
-            <tr>
-                <td>${i}</td>
-                <td>${iem.toFixed(2)}</td>
-                <td>${jrs.toFixed(2)}</td>
-                <td>${idem.toFixed(2)}</td>
-                <td>${vlr.toFixed(2)}</td>
-            </tr>
-            `;
+function price(){
+    let v = parseFloat(document.getElementById('v').value);
+    let i = parseFloat(document.getElementById('i').value);
+    let n = parseFloat(document.getElementById('n').value);
+    let juros = v * i;
+    let p = (v * Math.pow(1+i , n) * i)/(Math.pow(1+i , n)-1);
+    let amor = p - juros;
+    let corpo = document.querySelector('#tabela tbody');
+    let linha1 =`
+                <tr>
+                    <td>0</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td>${v.toFixed(2)}</td>
+                </tr>
+                `;
 
-        crp_tbl.innerHTML += lnha;
+    corpo.innerHTML = linha1;
+
+        for (let par=1; par<=n; par++){
+
+            let linha =`
+                <tr>
+                    <td>${par}</td>
+                    <td>${p.toFixed(2)}</td>
+                    <td>${juros.toFixed(2)}</td>
+                    <td>${amor.toFixed(2)}</td>
+                    <td>${(v-amor).toFixed(2)}</td>
+                </tr>
+                `;
+
+            corpo.innerHTML += linha;
+            v = v - amor;
+            juros = v * i;
+            amor = p - juros;
+
     }
+
 }
